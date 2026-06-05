6a611270f8a6d5ecaaccd1ecfebf4df0fb2c9cf2
Ranju Raveendran <39932642+ranjurave@users.noreply.github.com>
2026-06-05 15:09:42 +1200

    image display and css and js files organised

---
+++ static/js/script.js
@@ -0,0 +1,15 @@
+    function toggleimage(imageid, buttonid) {
+        const img = document.getElementById(imageid);
+        const btn = document.getElementById(buttonid);
+
+        document.querySelectorAll('img').forEach(img => {
+            img.style.display = 'none';
+        });
+
+        document.querySelectorAll('button').forEach(btn => {
+            btn.classList.remove('clicked');
+        });
+
+        btn.classList.add('clicked');
+        img.style.display = 'block';
+    }
