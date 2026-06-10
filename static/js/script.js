05ffdbb30180bcfd809e8640bf74fb6f7001e487
Ranju Raveendran <39932642+ranjurave@users.noreply.github.com>
2026-06-10 15:36:34 +1200

    bug fix

--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,12 @@
-    function toggleimage(imageid, buttonid) {
-        const img = document.getElementById(imageid);
-        const btn = document.getElementById(buttonid);
+function toggleimage(imageid, buttonid) {
+    const img = document.getElementById(imageid);
+    const btn = document.getElementById(buttonid);
 
+    if(btn.classList.contains('clicked')){
+        btn.classList.remove('clicked');
+        img.style.display = 'none';
+    }
+    else{
         document.querySelectorAll('img').forEach(img => {
             img.style.display = 'none';
         });
@@ -13,3 +18,15 @@
         btn.classList.add('clicked');
         img.style.display = 'block';
     }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const currenturl = window.location.href;
+    const navLinks = document.querySelectorAll(".nav-link");
+
+    navLinks.forEach(link => {
+        if (link.href === currenturl) {
+            link.classList.add("active");
+        }
+    });
+});
