dfc283df6f03101ccf486d2220fff213f7f5fd41
Ranju Raveendran <39932642+ranjurave@users.noreply.github.com>
2026-06-16 15:01:45 +1200

    wip points calculator

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, url_for, request, redirect, session
 
 app = Flask(__name__)
+app.secret_key = "my_secret_key";
 
 totalpoints = 0;
-
 
 @app.route("/", methods=["GET", "POST"])
 @app.route("/index")
@@ -18,24 +18,31 @@ def user():
 def reference():
     return render_template('reference.html')
 
+@app.route("/calculate/<int:points>")
+def calculate(points):
+    global totalpoints
+    totalpoints = points
+    return redirect(url_for("builder"))
+
 @app.route("/builder")
 def builder():
-    result = ''
-    if request.method == "POST":
-        try:
-            expression = request.form["display"]
-            result = eval(expression)
-        except Exception as e:
-            result = "error"
-    return render_template('builder.html', result=result)
+    return render_template("builder.html", points=totalpoints)
 
-@app.route("/calculator", methods=["GET", "POST"])
-def calculator():
-    result = ''
-    if request.method == "POST":
-        try:
-            expression = request.form["display"]
-            result = eval(expression)
-        except Exception as e:
-            result = "error"
-    return render_template('calculator.html', result=result)
+    # if request.method == "POST":
+    #     try:
+    #         expression = request.form["display"]
+    #         result = eval(expression)
+    #     except Exception as e:
+    #         result = "error"
+    # return render_template('builder.html', result=result)
+
+# @app.route("/calculator", methods=["GET", "POST"])
+# def calculator():
+#     result = ''
+#     if request.method == "POST":
+#         try:
+#             expression = request.form["display"]
+#             result = eval(expression)
+#         except Exception as e:
+#             result = "error"
+#     return render_template('calculator.html', result=result)
